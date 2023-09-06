6d7ccb7be0e683b56137df87214daf6a2cd004a7
patrickbismack <patrickbisma19@gmail.com>
2023-09-06 14:05:32 -0400

    Sales Prediction

---
+++ src/JavaFundamentals/SalesPrediction.java
@@ -0,0 +1,11 @@
+package JavaFundamentals;
+
+public class SalesPrediction {
+    public static void main(String[] args) {
+        double percent = 0.62;
+
+        double sales = 4600000;
+
+        System.out.print("The east coast division will generate $" + (sales * percent));
+    }
+}
