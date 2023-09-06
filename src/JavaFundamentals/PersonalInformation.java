0fe49613d4a7966fedbd12eb1d864af0c063cceb
patrickbismack <patrickbisma19@gmail.com>
2023-09-06 13:48:17 -0400

    Personal Information

---
+++ src/JavaFundamentals/PersonalInformation.java
@@ -0,0 +1,24 @@
+package JavaFundamentals;
+
+public class PersonalInformation {
+    public static void main (String [] args){
+        String name = "John Smith";
+
+        String address = "123 Cherry Wood Lane";
+
+        String city = "Rackerville";
+
+        String state = "Ca";
+
+        String zipCode = "48360";
+
+        String telephoneNumber = "(1)9674634867";
+
+        String collegeMajor = "Computer Science";
+
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address + ", " + city + ", " + state + ", " + zipCode);
+        System.out.println("Telephone: " + telephoneNumber);
+        System.out.println("College Major: " + collegeMajor);
+    }
+}
