2c719dd432471557f6eb2c1d5f1b5a842415e7e7
patrickbismack <patrickbisma19@gmail.com>
2023-09-06 13:39:41 -0400

    Name and Initials

---
+++ src/JavaFundamentals/NameAndInitials.java
@@ -0,0 +1,20 @@
+package JavaFundamentals;
+
+public class NameAndInitials {
+    public static void main (String [] args){
+        String firstName = "Tim";
+
+        String middleName = "Scott";
+
+        String lastName = "Jackson";
+
+        char firstInitial = 'T';
+
+        char middleInitial = 'S';
+
+        char lastInitial = 'J';
+
+        System.out.print("My name is " + firstName + " " + middleName + " " + lastName + ", my initials would be " + firstInitial + middleInitial
+                + lastInitial + ".");
+    }
+}
