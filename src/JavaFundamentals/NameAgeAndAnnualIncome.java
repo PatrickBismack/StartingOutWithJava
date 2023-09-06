985a63f28ec7fc617998e5b5f83fb1e52baf7bad
patrickbismack <patrickbisma19@gmail.com>
2023-09-06 13:33:05 -0400

    Name, Age, and Annual Income

---
+++ src/JavaFundamentals/NameAgeAndAnnualIncome.java
@@ -0,0 +1,13 @@
+package JavaFundamentals;
+
+public class NameAgeAndAnnualIncome {
+    public static void main (String [] args){
+        int age = 20;
+
+        double annualPay = 100000;
+
+        String name = "Patrick Bismack";
+
+        System.out.print("My name is " + name + ", my age is " + age + " and I hope to earn $" + annualPay + " per year.");
+    }
+}
