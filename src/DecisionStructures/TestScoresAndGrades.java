c8de60ad4229e841bf715b363e0f4fef56e87b14
patrickbismack <patrickbisma19@gmail.com>
2023-09-07 15:24:55 -0400

    Test Scores and Grades

---
+++ src/DecisionStructures/TestScoresAndGrades.java
@@ -0,0 +1,43 @@
+package DecisionStructures;
+
+import java.util.Scanner;
+
+public class TestScoresAndGrades {
+    public static void main (String [] args){
+        double test1;
+
+        double test2;
+
+        double test3;
+
+        double average;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter your score for test one: ");
+            test1 = keyboard.nextDouble();
+
+        System.out.print("Enter your score for test two: ");
+            test2 = keyboard.nextDouble();
+
+        System.out.print("Enter your score for test three: ");
+            test3 = keyboard.nextDouble();
+
+            average = (test1 + test2 + test3) / 3;
+
+        if (average >= 90) {
+            System.out.print("You test average was " + average + "%, so your letter grade is an A");
+        }
+        else if (average >= 80 && average <= 89) {
+            System.out.print("You test average was " + average + "%, so your letter grade is a B");
+        }
+        else if (average >= 70 && average <= 79) {
+            System.out.print("You test average was " + average + "%, so your letter grade is a C");
+        }
+        else if (average >= 60 && average <= 69){
+            System.out.print("You test average was " + average + "%, so your letter grade is a D");
+        }
+        else {
+            System.out.print("You test average was " + average + "%, so your letter grade is an E");
+        }
+    }
+}
