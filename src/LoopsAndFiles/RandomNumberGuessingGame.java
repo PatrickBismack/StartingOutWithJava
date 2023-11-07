1c4d8b9f77d88e518746fc3443bc37a5605ba798
patrickbismack <patrickbisma19@gmail.com>
2023-11-07 14:59:10 -0500

    added a counter

--- src/LoopsAndFiles/RandomNumberGuessingGame.java
+++ src/LoopsAndFiles/RandomNumberGuessingGame.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class RandomNumberGuessingGame {
     public static void main(String [] args){
         int guess;
+        int count = 1;
         int max = 100;
         int min = 0;
         int range = max - min + 1;
@@ -22,17 +23,19 @@ public class RandomNumberGuessingGame {
                     max = guess;
                 }
                 System.out.println("You guessed to high. Enter another guess between whole number " + min + " to " + max + ".");
-                guess = keyboard.nextInt();
+                    guess = keyboard.nextInt();
+                count++;
             }
             else if (guess < number){
                 if(guess > min){
                     min = guess;
                 }
                 System.out.println("You guessed to low. Enter another guess between whole number " + min + " to " + max + ".");
-                guess = keyboard.nextInt();
+                    guess = keyboard.nextInt();
+                count++;
             }
             else {
-                System.out.print("You guessed the right number.");
+                System.out.print("You guessed the right number in " + count + " turns.");
                 found = true;
             }
         }
